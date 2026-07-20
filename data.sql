23acdd7e73edd5edff82fdf0b817d33ec15286c3
apdi fatah <abdifatah@gmail.com>
2026-07-20 08:11:10 +0300

    112 inset data

--- data.sql
+++ data.sql
@@ -4,4 +4,11 @@ create table students (
     name VARCHAR(100) NOT NULL,
     age INT NOT NULL,
     grade VARCHAR(10) NOT NULL
+
  );
+ insert into students (name, age, grade) values
+('Alice', 14, '9th'),
+('Bob', 15, '10th'),
+('Charlie', 13, '8th'),
+('David', 16, '11th'),
+('Eva', 14, '9th');
